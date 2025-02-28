37a4afdef2dc9a5070b0aa28055c186795b4184a
helelys <tallyslabanca20@gmail.com>
2025-02-27 22:18:18 -0300

    Commited

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,17 @@ public class Main {
         * 1º) Fazer um programa para ler os valores da largura e altura de um retângulo.
         * Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.
         * Usar uma classe como mostrado no projeto ao lado.
-        */
+
 
         Retangulo retangulo = new Retangulo(3.00 , 4.00);
 
         System.out.println(retangulo.area());
         System.out.println(retangulo.perimetro());
         System.out.println(retangulo.diagonal());
+
+        * */
+
+
 
     }
 }
